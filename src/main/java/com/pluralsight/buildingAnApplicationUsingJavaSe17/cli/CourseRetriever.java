a65f56376b9cfbcadb71ca2b380e88a2d801eda9
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-02-02 13:14:10 -0600

    Added dependencies SLF4J-api and SLF4J-simple

--- src/main/java/com/pluralsight/buildingAnApplicationUsingJavaSe17/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/buildingAnApplicationUsingJavaSe17/cli/CourseRetriever.java
@@ -1,4 +1,30 @@
 package com.pluralsight.buildingAnApplicationUsingJavaSe17.cli;
 
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.Logger;
+
 public class CourseRetriever {
-}
+
+    public static final org.slf4j.Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String[] args){
+        LOG.info("CourseRetriever starting");
+        if (args.length==0){
+            LOG.warn("Please provide an author name as first argument.");
+            return;
+        };
+
+        try{
+            retrieveCourses(args[0]);
+        } catch (Exception exception) {
+            LOG.error("Unexpected error", exception);
+        };
+
+    }
+
+    private static void retrieveCourses(String authorId) {
+        LOG.info("Retrieving courses for author '{}'", authorId);
+    };
+
+};
