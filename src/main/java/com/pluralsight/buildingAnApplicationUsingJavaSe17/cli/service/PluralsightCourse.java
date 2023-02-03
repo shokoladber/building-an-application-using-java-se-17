ac62f75461026adc4576e2f7b2f1b8f12d999d99
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-02-03 16:34:46 -0600

    Writing the First Unit Test--started

--- src/main/java/com/pluralsight/buildingAnApplicationUsingJavaSe17/cli/service/PluralsightCourse.java
+++ src/main/java/com/pluralsight/buildingAnApplicationUsingJavaSe17/cli/service/PluralsightCourse.java
@@ -2,6 +2,17 @@ package com.pluralsight.buildingAnApplicationUsingJavaSe17.cli.service;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record PluralsightCourse(String id, String title, String duration, String contentUrl, Boolean isRetired) {
+
+    public long durationInMinutes(){
+        return Duration.between(
+                LocalTime.MIN,
+                LocalTime.parse(duration())
+        ).toMinutes();
+    };
+
 }
