1b1f0a3072598fe7aedac5b52f1e0946a2868f5e
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-02-07 22:33:00 -0600

    Setting up the Repository with H2 & JDBC

--- building-an-application-using-java-se-17-repository/src/main/java/com/pluralsight/courseinfo/domain/Course.java
+++ building-an-application-using-java-se-17-repository/src/main/java/com/pluralsight/courseinfo/domain/Course.java
@@ -1,4 +1,17 @@
 package com.pluralsight.courseinfo.domain;
 
-public record Course() {
+public record Course(String id, String name, long length, String url ) {
+
+    public Course{
+        filled(id);
+        filled(name);
+        filled(url);
+    }
+
+    private static void filled(String str){
+        if (str == null || str.isBlank()){
+            throw new IllegalArgumentException("No value present!");
+        }
+    }
+
 }
