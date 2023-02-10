3c93c2e004bc2cee6874823017a8eb086d07c442
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-02-10 15:23:09 -0600

    Returning JSON with JAX-RS and Jersey

--- building-an-application-using-java-se-17-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
+++ building-an-application-using-java-se-17-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
@@ -2,11 +2,17 @@ package com.pluralsight.courseinfo.server;
 
 import com.pluralsight.courseinfo.domain.Course;
 import com.pluralsight.courseinfo.repository.CourseRepository;
+import com.pluralsight.courseinfo.repository.RepositoryException;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Path("/courses")
@@ -20,12 +26,18 @@ public class CourseResource {
     }
 
     @GET
-    public String getCourses(){
-        return courseRepository
-                .getAllCourses()
-                .stream()
-                .map(Course::toString)
-                .collect(Collectors.joining(", "));
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Course> getCourses(){
+        try{
+            return courseRepository
+                    .getAllCourses()
+                    .stream()
+                    .sorted(Comparator.comparing(Course::id))
+                    .toList();
+        } catch (RepositoryException error){
+            LOG.error("Could not retrieve courses from the database", error);
+            throw new NotFoundException();
+        }
     }
 
 }
