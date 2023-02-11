04a6e2344db3d0bacad334ec797da04314a071f8
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-02-11 14:46:22 -0600

    Introducing External Application Configuration

--- building-an-application-using-java-se-17-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
+++ building-an-application-using-java-se-17-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
@@ -7,7 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.Properties;
 import java.util.logging.LogManager;
 
 public class CourseServer {
@@ -21,10 +24,22 @@ public class CourseServer {
     private static final String BASE_URI = "http://localhost:8080/";
 
     public static void main(String[] args){
-        LOG.info("Starting HTTP server");
-        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        String databaseFilename = loadDatabaseFilename();
+        LOG.info("Starting HTTP server with database {}", databaseFilename);
+        CourseRepository courseRepository = CourseRepository.openCourseRepository(databaseFilename);
         ResourceConfig config = new ResourceConfig().register(new CourseResource(courseRepository));
 
         GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
      }
+
+    private static String loadDatabaseFilename() {
+        try (InputStream propertiesStream =
+                CourseServer.class.getResourceAsStream("/server.properties")) {
+            Properties properties = new Properties();
+            properties.load(propertiesStream);
+            return properties.getProperty("course-info.database");
+        } catch (IOException error) {
+            throw new IllegalStateException("Could not load database filename");
+        }
+    }
 }
